86c098dfa1ca6809b26a6048d57d0a444d1d20e2
HARSH RAJ <harshrajssm130102@gmail.com>
2023-09-11 14:48:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,91 @@
+/* GSAP is a JavaScript library for building high-performance animations that work in **every** major 
+   browser. Animate CSS, SVG, canvas, React, Vue, WebGL, colors, strings, motion.
+   No other library delivers such advanced sequencing, reliability, and tight control while solving real-world problems on millions of sites. GSAP works around countless browser inconsistencies; your animations **just work**. At its core, GSAP is a high-speed property manipulator, updating values over time with extreme accuracy. It's up to 20x faster than jQuery! 
+*/
+
+var crsr = document.querySelector("#cursor")
+var blur = document.querySelector("#cursor-blur")
+
+document.addEventListener("mousemove",function(dets){
+    crsr.style.left = dets.x + "px"
+    crsr.style.top = dets.y + "px"
+    blur.style.left = dets.x -250 + "px"
+    blur.style.top = dets.y -250 + "px"
+})
+
+gsap.to("#nav" , { // purple bracket ke andar nav id pr jo property set krna hai kr do
+    backgroundColor : "#000",
+    height:"120px",
+    duration:0.5,
+    scrollTrigger:{
+        trigger:"#nav",
+        scroller:"body",
+        // markers:true,
+        start:"top -10%",
+        end:"top -11%",
+        scrub:1,
+    },
+});
+gsap.to("#main",{
+    backgroundColor: "#000",
+    scrollTrigger:{
+        trigger: "#main",
+        scroller:"body",
+        // markers:true,
+        start:"top -25%",
+        end:"top -70%",
+        scrub:2,
+    },
+});
+
+gsap.from("#about-us img,#about-us-info" , {
+    y:90,
+    opacity:0,
+    duration:1,
+    scrollTrigger:{
+        trigger:"#about-us",
+        scroller:"body",
+        // markers:true,
+        start:"top 70%",
+        end:"top 65%",
+        scurb:1,
+    },
+});
+
+gsap.from("#col1",{
+    y:-70,
+    x:-70,
+    scrollTrigger:{
+        trigger:"#col1",
+        scroller:"body",
+        // markers:true,
+        start:"top 55%",
+        end:"top 45%",
+        scrub:3
+    },
+});
+
+gsap.from("#col2",{
+    y:70,
+    x:70,
+    scrollTrigger:{
+        trigger:"#col1",
+        scroller:"body",
+        // markers:true,
+        start:"top 55%",
+        end:"top 45%",
+        scrub:3
+    },
+});
+
+gsap.from("#page4 h1",{
+    y:50,
+    scrollTrigger:{
+        trigger:"#page4 h1",
+        scroller:"body",
+        // markers:true,
+        start:"top 75%",
+        end:"top 70%",
+        scrub:3
+    },
+});
